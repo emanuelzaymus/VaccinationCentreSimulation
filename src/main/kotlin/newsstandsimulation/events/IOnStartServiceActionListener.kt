c01e6ba15ee0cd671bb2d871507740e749523665
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2021-03-26 15:35:51 +0100

    NewsStandSimulation removed

--- src/main/kotlin/newsstandsimulation/events/IOnStartServiceActionListener.kt
+++
@@ -1,5 +0,0 @@
-package newsstandsimulation.events
-
-interface IOnStartServiceActionListener {
-    fun handleOnStartService(actionSource: IOnStartServiceActionSource)
-}
--- src/main/kotlin/newsstandsimulation/events/IOnStartServiceActionSource.kt
+++
@@ -1,8 +0,0 @@
-package newsstandsimulation.events
-
-import newsstandsimulation.Customer
-
-interface IOnStartServiceActionSource {
-    fun setOnStartServiceActionListener(listener: IOnStartServiceActionListener)
-    fun customer(): Customer
-}
